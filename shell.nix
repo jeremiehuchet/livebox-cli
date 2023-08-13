6dd4bec1c958063a0a10e8848b2b8897c8da48b2
Jeremie Huchet <jeremiehuchet@users.noreply.github.com>
2023-08-13 14:05:13 +0200

    ⬆️ Update nix-shell

--- shell.nix
+++ shell.nix
@@ -1,21 +1,13 @@
 let
-  # release 22.11, 2023-05-19
-  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/03f2eb03bf67cda480680f82782889afa8d85b18.tar.gz")) {};
-  openapi-generator-cli-6_4 = pkgs.openapi-generator-cli.overrideAttrs (finalAttrs: previousAttrs: {
-    version = "6.4.0";
-    src = pkgs.fetchurl {
-      url = "mirror://maven/org/openapitools/${previousAttrs.pname}/${finalAttrs.version}/${previousAttrs.pname}-${finalAttrs.version}.jar";
-      sha256 = "sha256-Na6tMA4MlGn72dMM9G9BU4l9yygpEgkcpOySEtzp0VE=";
-    };
-  });
+  # release 23.05, 2023-08-13
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/90497216e09e9fe341fc3f2544398000cad33d20.tar.gz")) {};
 in pkgs.mkShell {
   buildInputs = with pkgs; [
-    openapi-generator-cli-6_4
     openssl
     pkg-config
-    rust_1_66.packages.stable.cargo
-    rust_1_66.packages.stable.rustc
-    rust_1_66.packages.stable.rustfmt
+    rust.packages.stable.cargo
+    rust.packages.stable.rustc
+    rust.packages.stable.rustfmt
     vscode
   ];
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
